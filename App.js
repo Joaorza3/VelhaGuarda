6128dc3a2969c534f4fee2ae94590aa8b600112e
joaorza3 <rozenaldofilho@gmail.com>
2021-04-15 11:17:42 -0300

    Tela "Cuidados" criada. No diretório: "src\pages\Home\Cuidados" O arquivo "index.js" contém todo o código fonte da tela, e na pasta "Assets" está todas as imagens usadas.

--- App.js
+++ App.js
@@ -2,8 +2,28 @@
 import React from "react";
 /// Importando as Rotas para a navegação react-navigation
 import Routes from "./src/routes/StackNavigation";
+import { 
+	Text,
+	View,
+	ScrollView,
+ } from 'react-native'
+
+
+import Cuidados from './src/pages/Home/Cuidados/index';
 
 export default function App() {
-  //Retornando as rotas
-  return <Routes />;
+
+	return (
+	<View>
+		
+		{/* Tela de Cuidados */}
+		<ScrollView>
+			<Cuidados />
+		</ScrollView>
+		
+
+		{/* //Retornando as rotas */}
+		<Routes />
+	</View>
+	);
 }
